9ed823dac482219ee2123b4955be33326a09c515
DevArchitectRB1X <140788423+DevArchitectRB1X@users.noreply.github.com>
2025-10-22 15:18:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ app.post("/api/:collection", async (req, res) => {
 
 app.post("/api/:collection/:id", async (req, res) => {
     try {
-        const { collection, factiune } = req.params;
+        const { collection, id } = req.params;
         const data = req.body;
 
         if (!data || Object.keys(data).length === 0)
@@ -105,3 +105,4 @@ app.delete("/api/:collection/:id", async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
+
